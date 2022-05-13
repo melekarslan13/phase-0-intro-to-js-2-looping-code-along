379febf3bd2166f4a021f49c49e77b95f6d751cc
melekarslan13 <melekarslan.ie@gmail.com>
2022-05-13 22:32:51 +0300

    loop assignment is done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names,event) {
+     // the initialization moves OUTSIDE the body of the loop!
+   const arr=[];
+    for (let i = 0; i< names.length;i++) {
+      arr[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+       // the iteration moves INSIDE the body of the loop!
+    }
+  
+    return arr;
+  }
+  function countDown(num){
+      while(num>=0){
+      console.log(num)
+   num--
+
+  }}
